7c3dd0d73fda4b75442088eb2f17bd29a8e99a75
dtitar <dtitar@natera.com>
2024-09-22 21:38:14 +0200

    hw-3-2.1,2,3 update UsersQueueExtension to work with Map<UserType, StaticUser>

--- niffler-dtitar-e2e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UsersQueueExtension.java
+++ niffler-dtitar-e2e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UsersQueueExtension.java
@@ -9,10 +9,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Optional;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -40,46 +37,50 @@ public class UsersQueueExtension implements BeforeEachCallback, AfterEachCallbac
 
 
     @Override
+    @SuppressWarnings("unchecked")
     public void beforeEach(ExtensionContext context) throws Exception {
         Arrays.stream(context.getRequiredTestMethod()
                              .getParameters())
               .filter(p -> AnnotationSupport.isAnnotated(p, UserType.class))
-              .findFirst()
               .map(p -> p.getAnnotation(UserType.class))
-              .ifPresent(ut -> {
-                             Optional<StaticUser> user = Optional.empty();
-                             StopWatch sw = StopWatch.createStarted();
-                             while (user.isEmpty() && sw.getTime(TimeUnit.SECONDS) < 30) {
-                                 user = ut.empty()
-                                         ? Optional.ofNullable(EMPTY_USERS.poll())
-                                         : Optional.ofNullable(NOT_EMPTY_USERS.poll());
-                             }
-                             Allure.getLifecycle()
-                                   .updateTestCase(testCase -> {
-                                       testCase.setStart(new Date().getTime());
-                                   });
-                             user.ifPresentOrElse(
-                                     u -> {
-                                         context.getStore(NAMESPACE)
-                                                .put(context.getUniqueId(), u);
-
-                                     },
-                                     () -> {
-                                         throw new IllegalStateException("Can't find user after 30 seconds");
-                                     }
-                             );
-                         }
-              );
+              .forEach(ut -> {
+                  Optional<StaticUser> user = Optional.empty();
+                  StopWatch sw = StopWatch.createStarted();
+                  while (user.isEmpty() && sw.getTime(TimeUnit.SECONDS) < 30) {
+                      user = ut.empty()
+                              ? Optional.ofNullable(EMPTY_USERS.poll())
+                              : Optional.ofNullable(NOT_EMPTY_USERS.poll());
+                  }
+                  Allure.getLifecycle()
+                        .updateTestCase(testCase -> {
+                            testCase.setStart(new Date().getTime());
+                        });
+                  user.ifPresentOrElse(
+                          u ->
+                                  ((Map<UserType, StaticUser>) context.getStore(NAMESPACE)
+                                                                      .getOrComputeIfAbsent(
+                                                                              context.getUniqueId(),
+                                                                              key -> new HashMap<>()
+                                                                      )).put(ut, u),
+                          () -> {
+                              throw new IllegalStateException("Can`t obtain user after 30s.");
+                          }
+                  );
+              });
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void afterEach(ExtensionContext context) throws Exception {
-        StaticUser user = context.getStore(NAMESPACE)
-                                 .get(context.getUniqueId(), StaticUser.class);
-        if (user.empty()) {
-            EMPTY_USERS.add(user);
-        } else {
-            NOT_EMPTY_USERS.add(user);
+        Map<UserType, StaticUser> map = context.getStore(NAMESPACE)
+                                               .get(context.getUniqueId(), Map.class);
+        for (Map.Entry<UserType, StaticUser> entry : map.entrySet()) {
+            StaticUser user = entry.getValue();
+            if (user.empty()) {
+                EMPTY_USERS.add(user);
+            } else {
+                NOT_EMPTY_USERS.add(user);
+            }
         }
     }
 
@@ -93,7 +94,9 @@ public class UsersQueueExtension implements BeforeEachCallback, AfterEachCallbac
 
     @Override
     public StaticUser resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-        return extensionContext.getStore(NAMESPACE)
-                               .get(extensionContext.getUniqueId(), StaticUser.class);
+        return (StaticUser) extensionContext.getStore(NAMESPACE)
+                                            .get(extensionContext.getUniqueId(), Map.class)
+                                            .get(AnnotationSupport.findAnnotation(parameterContext.getParameter(), UserType.class)
+                                                                  .get());
     }
 }
